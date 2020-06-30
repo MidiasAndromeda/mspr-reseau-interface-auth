c471ee8a931b5506bcf37977df8a24f17ae37491
Antoine PLAGNOL <antoine.plagnol@gmail.com>
2020-06-30 19:25:25 +0200

    efz

--- Mspr.Reseau.Auth.AdServices/AdServices.cs
+++ Mspr.Reseau.Auth.AdServices/AdServices.cs
@@ -302,7 +302,7 @@ namespace Mspr.Reseau.Auth.AdServices
 
             try
             {
-                SmtpClient client = new SmtpClient("127.0.0.1", 25);
+                SmtpClient client = new SmtpClient("192.168.1.37", 25);
                 client.UseDefaultCredentials = false;
                 // Credentials are necessary if the server requires the client
                 // to authenticate before it will send email on the client's behalf.
